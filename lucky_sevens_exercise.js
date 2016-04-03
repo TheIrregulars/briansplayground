08c074c95edba9d87183349e456e2bb18d79602a
Brian Charlton <BCharl24@Brians-MBP.home>
2016-04-03 14:16:07 -0400

    tweeking exercise code

---
+++ lucky_sevens_exercise.js
@@ -0,0 +1,13 @@
+var lucky_sevens = function (x) {
+	for (var i = 0; i <= x.length; i ++) {
+		console.log("i " + i)
+		console.log("sum " + (x[i] + x[i + 1] + x[i + 2]))
+		if ((x[i] + x[i + 1] + x[i + 2]) === 7) {
+			console.log(true)
+		}
+		else {
+		console.log(false)
+		}
+	}
+};
+lucky_sevens([2,1,5,1,0])
