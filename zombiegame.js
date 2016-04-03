281012c452a5ca711207e50f99156ff5981732d4
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-03 13:07:02 -0400

    added zombie object, added zombie hp status, fixed hitzombie bug

--- zombiegame.js
+++ zombiegame.js
@@ -1,20 +1,28 @@
 var headShot = true;
 var totalDamage = 0;
-while(headShot) {
+
+var zombie = {
+    hp:10,
+    attack:2
+}
+
+while(zombie.hp>0) {
     var hitZombie = Math.floor(Math.random() * 2)
     var hitsThisRound = Math.floor(Math.random()*5 + 1)
-    if (hitZombie) {
-        console.log("Ew, it ate you!");
-    }
-    else {
+    if (hitZombie) { 
         console.log("You hit the undead fucker!");
         totalDamage += hitsThisRound;
         if (totalDamage >= 2){
             console.log("You put a bullet in it's head!");
+            zombie.hp=0
         }
         else {
+            zombie.hp=zombie.hp-hitsThisRound
             console.log(hitZombie);
         }
     }
-    headShot = false;
+    else {
+        console.log("Ew, it ate you!");
+    }
+    console.log("zombie hp:" + zombie.hp)
 }
