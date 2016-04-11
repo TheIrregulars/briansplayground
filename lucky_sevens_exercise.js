3805b38fed696297343dd1ff5efc9f10a204517b
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-11 11:46:29 -0400

    erased print buffers; added more examples

--- lucky_sevens_exercise.js
+++ lucky_sevens_exercise.js
@@ -1,7 +1,5 @@
 var lucky_sevens = function (x) {
 	for (var i = 0; i <= x.length; i ++) {
-		console.log("i " + i)
-		console.log("sum " + (x[i] + x[i + 1] + x[i + 2]))
 		if ((x[i] + x[i + 1] + x[i + 2]) === 7) {
 			console.log(true)
 		}
@@ -10,4 +8,7 @@ var lucky_sevens = function (x) {
 		}
 	}
 };
-lucky_sevens([2,1,5,1,1,5,1,5])
+lucky_sevens([2,1,5,1,0])
+lucky_sevens([0,-2,1,8])
+lucky_sevens([7,7,7,7])
+lucky_sevens([3,4,3,4])
